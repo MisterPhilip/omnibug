219343848227b47589e2f9ee4bd3643e2d4e9717
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2007-09-27 06:44:46 +0000

    Update minor version; use separate major/minor/increment fields

--- deploy.bash
+++ deploy.bash
@@ -13,13 +13,14 @@ echo ""
 
 APP=omnibug
 MAJOR=0
-MINOR=`svn info ${PLACEHOLDER} |grep ^Revision|awk '{ print $2 }'`
+MINOR=4
+INC=`svn info ${PLACEHOLDER} |grep ^Revision|awk '{ print $2 }'`
 
 # update revision
-echo -n "$0: incrementing version: old=$MINOR; "
-MINOR=$((MINOR+1))
-VER="${MAJOR}.${MINOR}"
-echo "new=${MINOR}"
+echo -n "$0: incrementing version: old=$INC; "
+INC=$((INC+1))
+VER="${MAJOR}.${MINOR}.${INC}"
+echo "new=${INC}"
 echo ""
 cat install.rdf | sed "s/em:version>.*</em:version>${VER}</" > install.rdf
 
@@ -28,22 +29,22 @@ echo "Comitting updated install.rdf"
 svn commit -m"[$0] Incrementing revision for build" install.rdf
 echo ""
 
-OMNI=omnibug-${VER}.xpi
+XPI=omnibug-${VER}.xpi
 
 cat update.rdf.tpl | sed "s/XXX/${VER}/g" > update.rdf
-cp omnibug.xpi $OMNI
+cp omnibug.xpi $XPI
 
 echo "Adding updated install.rdf to omnibug.xpi"
-zip -u $OMNI
+zip -u $XPI
 echo ""
 
 if [[ "x$1" == "x" ]]; then
     echo "Sending update.rdf and xpi to galactica"
-    scp update.rdf $OMNI rosssimpson@galactica.7mph.com:httpdocs/dev/
+    scp update.rdf $XPI rosssimpson@galactica.7mph.com:httpdocs/dev/
     echo ""
 
     echo "Updating symlink"
-    ssh rosssimpson@galactica.7mph.com "rm httpdocs/dev/omnibug-current.xpi; ln -s $OMNI httpdocs/dev/omnibug-current.xpi"
+    ssh rosssimpson@galactica.7mph.com "ln -sf $XPI httpdocs/dev/omnibug-current.xpi"
 fi
 
 echo "Done."
