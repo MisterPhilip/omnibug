4c80d8ea155187c609385e15f57660aa0a0dc57b
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2007-09-27 06:40:44 +0000

    Don't bother creating jar for chrome; it isn't used. @TODO: implement someday?

--- build.bash
+++ build.bash
@@ -5,22 +5,24 @@
 #
 
 APP=omnibug
+XPIFILE=${APP}.xpi
 
-find . -name "*.jar" -exec rm {} \;
+#find . -name "*.jar" -exec rm {} \;
 find . -name "*.xpi" -exec rm {} \;
 
-echo "Creating chrome jar"
-cd chrome
-files=`find . -type f|egrep -v "(\.svn|\.jar)"`
-zip ${APP}.jar $files
-cd ..
-echo ""
+#echo "Creating chrome jar"
+#cd chrome
+#files=`find . -type f|egrep -v "(\.svn|\.jar)"`
+#zip ${APP}.jar $files
+#cd ..
+#echo ""
 
 echo "Creating xpi"
-xpifile=${APP}.xpi
-zip "$xpifile" chrome/${APP}.jar install.rdf chrome.manifest
+files=`find chrome -type f|grep -v \.svn`
+
+zip "$XPIFILE" $files install.rdf chrome.manifest
 echo ""
 
 echo -n "Created file: "
-ls "$xpifile"
+ls "$XPIFILE"
 
