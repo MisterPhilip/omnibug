f64ffeecc2a8101166468da9ed09e07c8b1a89fd
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2007-09-30 09:06:36 +0000

    Move toggle into context

---
+++ chrome/content/omnibug/omnibugContext.js
@@ -0,0 +1,40 @@
+/*
+ * $Id$
+ * $URL$
+ */
+
+if ( typeof Omnibug == "undefined" ) {
+    var Omnibug = {};
+}
+
+Omnibug.OmnibugContext = function( panel ) {
+    dump( ">>> OmnibugContext: constructor, panel=" + panel + "\n" );
+};
+
+/**
+ * Toggle a row
+ * @param {HTMLElement} el link clicked to toggle
+ */
+Omnibug.OmnibugContext.prototype.toggle = function( el ) {
+    var i, img,
+        tr = el.parentNode.parentNode,
+        td = tr.getElementsByTagName( "td" ),
+        div = tr.getElementsByTagName( "div" )[0];
+
+    // change expand/collapse icon
+    for( i=0; i<td.length; ++i ) {
+        if( td[i].className.match( /exp/ ) ) {
+           img = td[i].getElementsByTagName( "img" )[0];
+           if( img ) {
+               img.src = "chrome://omnibug/skin/win/twisty" + ( img.src.match( /Closed/ ) ? "Open" : "Closed" ) + ".png";
+           }
+        }
+    }
+
+    // hide/show the content div
+    if( div.className.match( /hid/ ) ) {
+        div.className = 'reg';
+    } else {
+        div.className = 'hid';
+    }
+}
