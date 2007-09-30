192c726c44eabfafad0570a69b7b49257609237c
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2007-09-30 09:33:43 +0000

    New url pattern

--- defaults/preferences/omnibug.js
+++ defaults/preferences/omnibug.js
@@ -1,2 +1,3 @@
 // pattern to match in request url
-pref( "extensions.omnibug.urlPattern", "2o7|moniforce\.gif" );
+//pref( "extensions.omnibug.urlPattern", "2o7|moniforce\.gif" );
+pref( "extensions.omnibug.urlPattern", "/b/ss/|2o7|moniforce\.gif" );
