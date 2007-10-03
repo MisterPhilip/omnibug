7093f378516a0d75d36affd1735019d1cfde4318
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2007-10-03 07:25:27 +0000

    Rename urlPattern to defaultPattern; add enableFileLogging

--- defaults/preferences/omnibug.js
+++ defaults/preferences/omnibug.js
@@ -1,3 +1,4 @@
 // pattern to match in request url
 //pref( "extensions.omnibug.urlPattern", "2o7|moniforce\.gif" );
-pref( "extensions.omnibug.urlPattern", "/b/ss/|2o7|moniforce\.gif" );
+pref( "extensions.omnibug.defaultPattern", "/b/ss/|2o7|moniforce\.gif" );
+pref( "extensions.omnibug.enableFileLogging", false );
