e3f859bb3ee456551d5514aeca82de4546291d22
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2008-07-17 15:38:57 +0000

    Cleanup; CI helper for FF1.5

--- chrome/content/omnibug/md5.js
+++ chrome/content/omnibug/md5.js
@@ -9,40 +9,47 @@
 Md5Impl = ( function() {
     var _md5 = null,
         Pub = {
-        md5: function(str) {
-            if (null == _md5) {
+        md5: function( str ) {
+            if( null === _md5 ) {
                 return "";
             }
 
             // Build array of character codes to MD5
             var arr = [];
             var ii = str.length;
-            for (var i = 0; i < ii; ++i) {
-                arr.push(str.charCodeAt(i));
+            for( var i = 0; i < ii; ++i ) {
+                arr.push( str.charCodeAt( i ) );
             }
-            _md5.init(Ci.nsICryptoHash.MD5);
-            _md5.update(arr, arr.length);
-            var hash = _md5.finish(false);
+            _md5.init( CI( 'nsICryptoHash' ).MD5 );
+            _md5.update( arr, arr.length );
+            var hash = _md5.finish( false );
 
             // Unpack the binary data bin2hex style
             var ascii = [];
             ii = hash.length;
-            for (var i = 0; i < ii; ++i) {
-                var c = hash.charCodeAt(i);
+            for( var i = 0; i < ii; ++i ) {
+                var c = hash.charCodeAt( i );
                 var ones = c % 16;
                 var tens = c >> 4;
-                ascii.push(String.fromCharCode(tens + (tens > 9 ? 87 : 48)) +
-                    String.fromCharCode(ones + (ones > 9 ? 87 : 48)));
+                ascii.push( String.fromCharCode( tens + ( tens > 9 ? 87 : 48 ) ) +
+                    String.fromCharCode( ones + ( ones > 9 ? 87 : 48 ) ) );
             }
 
-            return ascii.join("");
+            return ascii.join( "" );
+        }
+    }
+
+    // Components.interfaces helper
+    if( typeof( "CI" ) !== "function" ) {
+        function CI( ifaceName ) {
+            return Components.interfaces[ifaceName];
         }
     }
 
     try {
-        _md5 = CC('@mozilla.org/security/hash;1').createInstance(Ci.nsICryptoHash);
-    } catch (err) {
-        Components.utils.reportError(err);
+        _md5 = CC( '@mozilla.org/security/hash;1' ).createInstance( CI( 'nsICryptoHash' ) );
+    } catch( err ) {
+        Components.utils.reportError( err );
     }
 
     return Pub;
