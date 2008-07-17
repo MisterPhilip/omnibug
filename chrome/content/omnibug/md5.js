6b66030bea4c01a29928a9033ac7cde8fb0d9082
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2008-07-17 15:10:01 +0000

    New md5.js file

---
+++ chrome/content/omnibug/md5.js
@@ -0,0 +1,49 @@
+/*
+ * Native MD5 in Firefox; fetched and modified slightly on 17-July-2008 from:
+ *     http://rcrowley.org/2007/11/15/md5-in-xulrunner-or-firefox-extensions/
+ *
+ * $Id$
+ * $URL$
+ */
+
+Md5Impl = ( function() {
+    var _md5 = null,
+        Pub = {
+        md5: function(str) {
+            if (null == _md5) {
+                return "";
+            }
+
+            // Build array of character codes to MD5
+            var arr = [];
+            var ii = str.length;
+            for (var i = 0; i < ii; ++i) {
+                arr.push(str.charCodeAt(i));
+            }
+            _md5.init(Ci.nsICryptoHash.MD5);
+            _md5.update(arr, arr.length);
+            var hash = _md5.finish(false);
+
+            // Unpack the binary data bin2hex style
+            var ascii = [];
+            ii = hash.length;
+            for (var i = 0; i < ii; ++i) {
+                var c = hash.charCodeAt(i);
+                var ones = c % 16;
+                var tens = c >> 4;
+                ascii.push(String.fromCharCode(tens + (tens > 9 ? 87 : 48)) +
+                    String.fromCharCode(ones + (ones > 9 ? 87 : 48)));
+            }
+
+            return ascii.join("");
+        }
+    }
+
+    try {
+        _md5 = CC('@mozilla.org/security/hash;1').createInstance(Ci.nsICryptoHash);
+    } catch (err) {
+        Components.utils.reportError(err);
+    }
+
+    return Pub;
+} )();
