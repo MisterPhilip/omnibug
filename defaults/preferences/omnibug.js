3303e59b205fbef8bdafc57bf164a645e77d9c7b
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2008-07-17 16:35:28 +0000

    Add highlightKeys and usefulKeys prefs

--- defaults/preferences/omnibug.js
+++ defaults/preferences/omnibug.js
@@ -2,3 +2,5 @@
 //pref( "extensions.omnibug.urlPattern", "2o7|moniforce\.gif" );
 pref( "extensions.omnibug.defaultPattern", "/b/ss/|2o7|moniforce\.gif" );
 pref( "extensions.omnibug.enableFileLogging", false );
+pref( "extensions.omnibug.usefulKeys", "pageName,ch,h1,purchaseID,events,products,pev2" );
+pref( "extensions.omnibug.highlightKeys", "events,products" );
