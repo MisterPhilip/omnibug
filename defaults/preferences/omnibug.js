f3384f32837fd953693ec4dbbd82fe3c9966ab8f
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2008-11-10 13:19:47 +0000

    Preliminary support for Google Analytics/Urchin

--- defaults/preferences/omnibug.js
+++ defaults/preferences/omnibug.js
@@ -1,5 +1,5 @@
 // pattern to match in request url
-pref( "extensions.omnibug.defaultPattern", "/b/ss/|2o7|moniforce\.gif|dcs\.gif" );
+pref( "extensions.omnibug.defaultPattern", "/b/ss/|2o7|moniforce\.gif|dcs\.gif|__utm\.gif" );
 pref( "extensions.omnibug.enableFileLogging", false );
 pref( "extensions.omnibug.usefulKeys", "pageName,ch,h1,purchaseID,events,products,pev2" );
 pref( "extensions.omnibug.highlightKeys", "events,products" );
