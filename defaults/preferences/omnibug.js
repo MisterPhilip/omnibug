3114b6f5cb78efb6da5eaf0c9b29205e63dea8c4
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2008-11-16 22:20:23 +0000

    Add new colors prefs

--- defaults/preferences/omnibug.js
+++ defaults/preferences/omnibug.js
@@ -3,5 +3,16 @@ pref( "extensions.omnibug.defaultPattern", "/b/ss/|2o7|moniforce\.gif|dcs\.gif|_
 pref( "extensions.omnibug.enableFileLogging", false );
 pref( "extensions.omnibug.usefulKeys", "pageName,ch,h1,purchaseID,events,products,pev2" );
 pref( "extensions.omnibug.highlightKeys", "events,products" );
+
+// show entries expanded?
 pref( "extensions.omnibug.alwaysExpand", false );
+
+// surround values with quotes?
 pref( "extensions.omnibug.showQuotes", true );
+
+// colors
+pref( "extensions.omnibug.color_load", "#dbedff" );
+pref( "extensions.omnibug.color_click", "#f1ffdb" );
+pref( "extensions.omnibug.color_prev", "#ffd5de" );
+pref( "extensions.omnibug.color_quotes", "#f00" );
+pref( "extensions.omnibug.color_hilite", "#ff0" );
