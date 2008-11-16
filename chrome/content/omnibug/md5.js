137fcc5f529b29ae61c4018c71b9fbcb2d7bef16
rsimpson <rsimpson@c5766d0b-dca0-a82e-4f71-5f540b519b94>
2008-11-16 18:18:14 +0000

    Surround with FBL.ns; add CC helper

--- chrome/content/omnibug/md5.js
+++ chrome/content/omnibug/md5.js
@@ -6,51 +6,63 @@
  * $URL$
  */
 
-Md5Impl = ( function() {
-    var _md5 = null,
-        Pub = {
-        md5: function( str ) {
-            if( null === _md5 ) {
-                return "";
-            }
 
-            // Build array of character codes to MD5
-            var arr = [];
-            var ii = str.length;
-            for( var i = 0; i < ii; ++i ) {
-                arr.push( str.charCodeAt( i ) );
-            }
-            _md5.init( CI( 'nsICryptoHash' ).MD5 );
-            _md5.update( arr, arr.length );
-            var hash = _md5.finish( false );
 
-            // Unpack the binary data bin2hex style
-            var ascii = [];
-            ii = hash.length;
-            for( var i = 0; i < ii; ++i ) {
-                var c = hash.charCodeAt( i );
-                var ones = c % 16;
-                var tens = c >> 4;
-                ascii.push( String.fromCharCode( tens + ( tens > 9 ? 87 : 48 ) ) +
-                    String.fromCharCode( ones + ( ones > 9 ? 87 : 48 ) ) );
-            }
+FBL.ns( function() { with( FBL ) {
+    Md5Impl = ( function() {
+        var _md5 = null,
+            Pub = {
+            md5: function( str ) {
+                if( null === _md5 ) {
+                    return "";
+                }
 
-            return ascii.join( "" );
+                // Build array of character codes to MD5
+                var arr = [];
+                var ii = str.length;
+                for( var i = 0; i < ii; ++i ) {
+                    arr.push( str.charCodeAt( i ) );
+                }
+                _md5.init( CI( 'nsICryptoHash' ).MD5 );
+                _md5.update( arr, arr.length );
+                var hash = _md5.finish( false );
+
+                // Unpack the binary data bin2hex style
+                var ascii = [];
+                ii = hash.length;
+                for( var i = 0; i < ii; ++i ) {
+                    var c = hash.charCodeAt( i );
+                    var ones = c % 16;
+                    var tens = c >> 4;
+                    ascii.push( String.fromCharCode( tens + ( tens > 9 ? 87 : 48 ) ) +
+                        String.fromCharCode( ones + ( ones > 9 ? 87 : 48 ) ) );
+                }
+
+                return ascii.join( "" );
+            }
         }
-    }
 
-    // Components.interfaces helper
-    if( typeof( "CI" ) !== "function" ) {
-        function CI( ifaceName ) {
-            return Components.interfaces[ifaceName];
+        // Components.classes helper
+        if( typeof( "CC" ) !== "function" ) {
+            function CC( className ) {
+                return Components.classes[className];
+            }
         }
-    }
 
-    try {
-        _md5 = CC( '@mozilla.org/security/hash;1' ).createInstance( CI( 'nsICryptoHash' ) );
-    } catch( err ) {
-        Components.utils.reportError( err );
-    }
+        // Components.interfaces helper
+        if( typeof( "CI" ) !== "function" ) {
+            function CI( ifaceName ) {
+                return Components.interfaces[ifaceName];
+            }
+        }
 
-    return Pub;
-} )();
+
+        try {
+            _md5 = CC( '@mozilla.org/security/hash;1' ).createInstance( CI( 'nsICryptoHash' ) );
+        } catch( err ) {
+            Components.utils.reportError( err );
+        }
+
+        return Pub;
+    } )();
+}} );
