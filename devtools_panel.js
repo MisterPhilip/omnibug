9e8b1c9d7e5f01903577d0635bfeb7e5df7da62f
Ross Simpson <simpsora@gmail.com>
2013-04-21 17:00:56 +1000

    Cleanup and apply js module pattern

--- devtools_panel.js
+++ devtools_panel.js
@@ -1,35 +1,32 @@
-
-alert("a");
 /*
  * Chromnibug
  * DevTools panel code (view)
  */
-window.bobo = (function() {
-//(function() {
-alert('in');
+window.Chromnibug = ( function() {
 
     // public
-    //return {
-	function show_message( msg ) {
-        //alert( "[devtools_panel.js] do_something got called with msg=" + msg );
-        //alert( "show_message called with msg=" + msg );
+    return {
+	    show_message: function ( msg ) {
+            //alert( "[devtools_panel.js] do_something got called with msg=" + msg );
+            //alert( "show_message called with msg=" + msg );
 
-        var table = document.getElementById( "chr_table" );
-        if( !! table ) {
-            var td1 = document.createElement( "td" );
-            td1.textContent = msg.requestId;
-            var td2 = document.createElement( "td" );
-            td2.textContent = msg.url + " (" + localStorage["favorite_color"] + ")"
-            var tr = document.createElement( "tr" );
-            tr.appendChild( td1 );
-            tr.appendChild( td2 );
-            var tbody = document.createElement( "tbody" );
-            tbody.appendChild( tr );
-            table.appendChild( tbody );
-        } else {
-            alert( "can't find table!" );
+            var table = document.getElementById( "chr_table" );
+            if( !! table ) {
+                var td1 = document.createElement( "td" );
+                td1.textContent = msg.requestId;
+                var td2 = document.createElement( "td" );
+                td2.textContent = msg.url + " (" + localStorage["favorite_color"] + ")"
+                var tr = document.createElement( "tr" );
+                tr.appendChild( td1 );
+                tr.appendChild( td2 );
+                var tbody = document.createElement( "tbody" );
+                tbody.appendChild( tr );
+                table.appendChild( tbody );
+            } else {
+                alert( "can't find table!" );
+            }
         }
-    }
+    };
 
     /*
     // for click events (?)
@@ -39,18 +36,5 @@ alert('in');
         //respond('Another stupid example!');
     };
     */
-
-
-//})();
-//})();
-}());
-
-alert("b");
-
-alert("bob=" + window.bobo );
-
-//alert('sm=' + show_message );
-
-//alert( "wc=" + window.Chromnibug );
-//alert( "wcs=" + window.Chromnibug.show_message );
+}() );
 
