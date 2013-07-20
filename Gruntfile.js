8890af94b7eac2839274a98f0ddf51b53f199471
Ross Simpson <simpsora@gmail.com>
2013-07-20 13:57:57 +1000

    Cleanup gruntfile; add makeFirefox

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,14 @@
 /*global module*/
 module.exports = function( grunt ) {
-    'use strict';
-
+    "use strict";
     var gruntConfig = {};
-    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+    grunt.initConfig( gruntConfig );
+
+
+    /*
+     * JS Lint
+     */
+    grunt.loadNpmTasks( "grunt-contrib-jshint" );
     gruntConfig.jshint = {
         options: { bitwise: true, camelcase: false, curly: true, eqeqeq: true, forin: true, immed: true,
                    indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: false,
@@ -11,57 +16,59 @@ module.exports = function( grunt ) {
                    white: false, laxcomma: true, nonstandard: true, browser: true, maxparams: 3, maxdepth: 4,
                    maxstatements: 50 },
         all: [
-            'Gruntfile.js',
-            'chrome/devtools.js'
-            //'chrome/devtools_panel.js'
-            //'common/providers.js'
-            //'chrome/eventPage.js'
-            //'chrome/options.js'
+            "Gruntfile.js",
+            "chrome/devtools.js"
+            //"chrome/devtools_panel.js"
+            //"common/providers.js"
+            //"chrome/eventPage.js"
+            //"chrome/options.js"
         ]
     };
 
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    /*
+     * Jasmine tests
+     */
+    grunt.loadNpmTasks("grunt-contrib-jasmine");
     gruntConfig.jasmine = {
         src: {
             src: [
-                //'src/js/**/*.js',
-                //'!src/js/**/*.test.js'
-                'common/*.js'
+                //"src/js/**/*.js",
+                //"!src/js/**/*.test.js"
+                "common/*.js"
             ],
             options: {
-                //specs: 'src/js/**/*.test.js',
-                specs: 'test/common/*.js'
+                //specs: "src/js/**/*.test.js",
+                specs: "test/common/*.js"
                 /*
                 junit: {
-                    path: 'output/testresults'
+                    path: "output/testresults"
                 }
                 */
             }
         }
     };
-    grunt.registerTask('test', 'jasmine:src');
+    grunt.registerTask("test", "jasmine:src");
+
 
     /*
     gruntConfig.jasmine.istanbul= {
         src: gruntConfig.jasmine.src.src,
         options: {
             specs: gruntConfig.jasmine.src.options.specs,
-            template: require('grunt-template-jasmine-istanbul'),
+            template: require("grunt-template-jasmine-istanbul"),
             templateOptions: {
-                coverage: 'output/coverage/coverage.json',
+                coverage: "output/coverage/coverage.json",
                 report: [
-                    {type: 'html', options: {dir: 'output/coverage'}},
-                    {type: 'cobertura', options: {dir: 'output/coverage/cobertura'}},
-                    {type: 'text-summary'}
+                    {type: "html", options: {dir: "output/coverage"}},
+                    {type: "cobertura", options: {dir: "output/coverage/cobertura"}},
+                    {type: "text-summary"}
                 ]
             }
         }
     };
-    grunt.registerTask('coverage', 'jasmine:istanbul');
+    grunt.registerTask("coverage", "jasmine:istanbul");
     */
-
-    grunt.initConfig( gruntConfig );
-    grunt.registerTask( 'travis', ['jshint', 'test' ]);
 
 
     // copies dependency files into place
@@ -98,5 +105,45 @@ module.exports = function( grunt ) {
     };
     grunt.registerTask( "makeChrome", [ "copy", "crx" ] );
 
+
+    /*
+     * Build Firefox extension
+     */
+    grunt.loadNpmTasks( "grunt-contrib-compress" );
+    gruntConfig.compress = {
+        site: {
+            options: {
+                archive: "foo.zip"
+            },
+            files: [
+                { expand: true, cwd: "firefox/", src: [ "chrome/**" ] },
+                { expand: true, cwd: "firefox/", src: [ "defaults/**" ] },
+                { expand: true, cwd: "firefox/", src: [ "chrome.manifest" ] },,
+                { expand: true, cwd: "firefox/", src: [ "install.rdf.site" ], rename: function( d, s ) {
+                    return s.replace( /\.site|\.amo/, "" );
+                } }
+            ]
+        },
+        amo: {
+            options: {
+                archive: "foo.zip"
+            },
+            files: [
+                { expand: true, cwd: "firefox/", src: [ "chrome/**" ] },
+                { expand: true, cwd: "firefox/", src: [ "defaults/**" ] },
+                { expand: true, cwd: "firefox/", src: [ "chrome.manifest" ] },,
+                { expand: true, cwd: "firefox/", src: [ "install.rdf.amo" ], rename: function( d, s ) {
+                    return s.replace( /\.site|\.amo/, "" );
+                } }
+            ]
+        }
+    };
+    grunt.registerTask( "makeFirefox", [ "copy", "compress:site" ] );
+
+    grunt.registerTask( "makeAll", [ "clean", "jshint", "test", "copy", "makeChrome", "makeFirefox" ] );
+
+
+    // CI tasks
+    grunt.registerTask( "travis", ["jshint", "test" ]);
 
 };
