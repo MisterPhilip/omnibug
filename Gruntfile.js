84b4ba43d0e4e7bfcf4654db61a7a405ba7361a1
Philip Lawrence <philip@misterphilip.com>
2018-08-16 17:21:17 -0700

    Host GA script internally

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
             },
             "production": {
                 "name": "Omnibug",
-                "version": "1.2.2",
+                "version": "1.2.3",
                 "storageKey": "omnibug",
                 "feedbackUrl": "https://omnibug.io/feedback",
                 "analyticsID": "UA-114343677-2"
