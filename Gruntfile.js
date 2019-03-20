69dffd108d82fcbcbc5a5c5fb53614c1eff18bd5
Philip Lawrence <philip@misterphilip.com>
2019-03-20 11:08:54 -0700

    Update for 1.4.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
             },
             "production": {
                 "name": "Omnibug",
-                "version": "1.4.0",
+                "version": "1.4.1",
                 "storageKey": "omnibug",
                 "feedbackUrl": "https://omnibug.io/feedback",
                 "analyticsID": "UA-114343677-2"
