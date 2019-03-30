6ff65ff2180fc30a47d3639e5468fe5ede8d2168
Philip Lawrence <philip@misterphilip.com>
2019-03-30 12:00:30 -0700

    Prep for 1.5.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
             },
             "production": {
                 "name": "Omnibug",
-                "version": "1.4.1",
+                "version": "1.5.0",
                 "storageKey": "omnibug",
                 "feedbackUrl": "https://omnibug.io/feedback",
                 "analyticsID": "UA-114343677-2"
