5112a193610ae8eeb96de49e4f5248a694420a97
Philip Lawrence <philip@misterphilip.com>
2019-04-14 14:19:37 -0700

    Change how POST data is handled by default

--- src/providers/BaseProvider.js
+++ src/providers/BaseProvider.js
@@ -163,20 +163,52 @@ class BaseProvider
      */
     parsePostData(postData = "")
     {
-        let params = [];
-        // Handle POST data first, if applicable (treat as query params)
+        let params = [],
+            parsed = {};
         if(typeof postData === "string" && postData !== "")
         {
-            let keyPairs = postData.split("&");
-            keyPairs.forEach((keyPair) => {
-                let splitPair = keyPair.split("=");
-                params.push([splitPair[0], decodeURIComponent(splitPair[1] || "")]);
-            });
-        } else if(typeof postData === "object") {
-            Object.entries(postData).forEach((entry) => {
-                // @TODO: consider handling multiple values passed?
-                params.push([entry[0], entry[1].toString()]);
-            });
+            try
+            {
+                parsed = JSON.parse(postData);
+                /* Based on https://stackoverflow.com/a/19101235 */
+                function recurse (cur, prop)
+                {
+                    if (Object(cur) !== cur)
+                    {
+                        params.push([prop, cur]);
+                    }
+                    else if (Array.isArray(cur))
+                    {
+                        for(var i=0, l=cur.length; i<l; i++)
+                        {
+                            recurse(cur[i], prop + "[" + i + "]");
+                        }
+                        if (l === 0)
+                        {
+                            params.push([prop, ""]);
+                        }
+                    }
+                    else
+                    {
+                        let isEmpty = true;
+                        for (let p in cur)
+                        {
+                            if(!cur.hasOwnProperty(p)) { continue; }
+                            isEmpty = false;
+                            recurse(cur[p], prop ? prop+"."+p : p);
+                        }
+                        if (isEmpty && prop)
+                        {
+                            params.push([prop, ""]);
+                        }
+                    }
+                }
+                recurse(parsed, "");
+            }
+            catch(e)
+            {
+                console.error("postData is not JSON", e.message);
+            }
         }
         return params;
     }
