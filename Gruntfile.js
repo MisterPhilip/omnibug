6a688003b066a0ba0573d1ef4050b74a44cc6bd4
Philip Lawrence <philip@misterphilip.com>
2020-11-07 12:09:44 -0700

    Prep 1.17.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
             },
             "production": {
                 "name": "Omnibug",
-                "version": "1.17.0",
+                "version": "1.17.1",
                 "storageKey": "omnibug",
                 "feedbackUrl": "https://github.com/MisterPhilip/omnibug/issues",
                 "analyticsID": "UA-114343677-2"
