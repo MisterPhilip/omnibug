040f180052833f3064c23716755ea4f20030851e
Sasindu Hewage <sasindu@cinergix.com>
2024-04-05 12:21:14 +0530

    Add indicative support

---
+++ src/providers/Indicative.js
@@ -0,0 +1,84 @@
+/**
+ * Indicative
+ * https://www.https://app.indicative.com/#/segmentation/1c282f09-03b3-4921-bb8b-2b7c4a4f7321.com/
+ *
+ * @class
+ * @extends BaseProvider
+ */
+class IndicativeProvider extends BaseProvider {
+    constructor() {
+        super();
+        this._key = "INDICATIVE";
+        this._pattern = /api\.indicative\.com\/service\/event/;
+        this._name = "Indicative";
+        this._type = "analytics";
+    }
+
+    /**
+     * Retrieve the column mappings for default columns (account, event type)
+     *
+     * @return {{}}
+     */
+    get columnMapping() {
+        return {
+            "account": "apiKey",
+            "requestType": "eventName"
+        };
+    }
+
+    /**
+     * Retrieve the group names & order
+     *
+     * @returns {*[]}
+     */
+    get groups() {
+        return [
+            {
+                "key": "events",
+                "name": "Event"
+            },
+            {
+                "key": "eventproperties",
+                "name": "Event Properties"
+            }
+        ];
+    }
+
+    /**
+     * Get all of the available URL parameter keys
+     *
+     * @returns {{}}
+     */
+    get keys() {
+        return {
+            "eventName": {
+                "name": "Event Name",
+                "group": "events"
+            },
+            "properties.value1Type": {
+                "name": "Value 1 Type",
+                "group": "eventproperties"
+            },
+            "properties.value1": {
+                "name": "Value 1",
+                "group": "eventproperties"
+            },
+            "properties.value2Type": {
+                "name": "Value 2 Type",
+                "group": "eventproperties"
+            },
+            "properties.value2": {
+                "name": "Value 2",
+                "group": "eventproperties"
+            },
+            "properties.value3Type": {
+                "name": "Value 3 Type",
+                "group": "eventproperties"
+            },
+            "properties.value3": {
+                "name": "Value 3",
+                "group": "eventproperties"
+            },
+        };
+    }
+}
